118c246741fdeb8bcf7dd8c15f5a4f5e1fed96a6
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-26 03:54:11 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -11,4 +11,17 @@ class Patient
   def self.all
     @@all
   end
+
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all.select { |app| app.patient == self }
+  end
+
+  def patients
+    self.appointments.map { |pat| pat.doctor }
+  end
+
 end
