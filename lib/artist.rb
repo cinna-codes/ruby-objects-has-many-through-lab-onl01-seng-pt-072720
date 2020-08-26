7bba00b172e06459b6f62b33d0d381ecc3a22aac
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-26 03:22:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,6 +13,7 @@ class Artist
   end
 
   def new_song(name, genre)
+    # takes in an argument of a name and genre creates a new song. That song should know that it belongs to the artist.
     name = Song.new(genre)
     name.artist = self
   end
